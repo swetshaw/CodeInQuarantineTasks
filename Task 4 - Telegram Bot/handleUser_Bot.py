1f5f35ffa53fa55f21e2d77c816b3a96f2c16de1
Sweta Shaw <swt.shw94@gmail.com>
2020-04-09 14:21:12 +0530

    Create handleUser_Bot.py

---
+++ Task 4 - Telegram Bot/handleUser_Bot.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 13:53:33 2020
+
+@author: Sweta Shaw
+"""
+
+import logging
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+
+
+updater = Updater(token='TOKEN', use_context=True)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def handle_new_chat_members(update, context):
+        msg = update.effective_message
+        try:
+            context.bot.delete_message(
+                chat_id=msg.chat.id,
+                message_id=msg.message_id,
+            )
+        except Exception as ex:
+            if 'Message to delete not found' in str(ex):
+                logging.error('Failed to delete msg: %s', ex)
+                return
+            elif "Message can't be deleted" in str(ex):
+                logging.error('Failed to delete msg: %s', ex)
+                return
+            else:
+                raise
+
+def handle_left_chat_member(update, context):
+        msg = update.effective_message
+        try:
+            context.bot.delete_message(
+                chat_id=msg.chat.id,
+                message_id=msg.message_id,
+            )
+        except Exception as ex:
+            if 'Message to delete not found' in str(ex):
+                logging.error('Failed to delete join message: %s' % ex)
+                return
+            elif "Message can't be deleted" in str(ex):
+                logging.error('Failed to delete msg: %s', ex)
+                return
+            else:
+                raise
+newMember_handler = MessageHandler(Filters.status_update.new_chat_members, handle_new_chat_members)
+dispatcher.add_handler(newMember_handler)
+
+leftUser_handler = MessageHandler(Filters.status_update.left_chat_member, handle_left_chat_member)
+dispatcher.add_handler(leftUser_handler)
+
+updater.start_polling()
+updater.idle()
